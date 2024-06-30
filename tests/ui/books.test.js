c879646c28fe9f601d8038a4ae67059a1fa15185
Rosen Dimitrov <rosen.dimitrov@test.com>
2024-06-30 14:25:21 +0300

    Fixed UI tests

--- tests/ui/books.test.js
+++ tests/ui/books.test.js
@@ -1,8 +1,8 @@
 const { test, expect } = require('@playwright/test');
 
 test('Check books page', async ({ page }) => {
-    await page.goto('http://localhost:8080/library');
-    const list = await page.$('ul');
+    await page.goto('http://localhost:8080/books');
+    const list = await page.$('li');
     expect(list).toBeTruthy();
   });
   
